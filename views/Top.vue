33c6171e956dcc39970f84c0d7171315c311cbfd
Seiko Horiuchi <seikohoriuchi@SeikonoMacBook-Pro.local>
2020-05-11 16:38:20 +0900

    add index UI

--- views/Top.vue
+++ views/Top.vue
@@ -44,58 +44,146 @@
       </div>
     </section>
     <section class="userMarket">
-      <h3 class="index_title">
-        <span class="index_title-en">User Market</span>
-        <span class="index_title-jp">ユーザー間取引</span>
-      </h3>
-      <ul class="userMarket_list">
-        <li class="userMarket_item">
-          <a href="">
-            <figure class="userMarket_img">
-              <img
-                src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
-                alt=""
-              />
-              <figcaption><span>あと</span>1<span>日</span>12:34:56</figcaption>
-            </figure>
-            <div class="userMarket_title">パラボラ</div>
-            <div class="userMarket_artist">Official髭男dism</div>
-            <div class="userMarket_price">
-              1,500
-              <span class="unit">円〜</span>
-            </div>
-          </a>
-        </li>
-      </ul>
-      <div class="pickupAuction_link">
-        <a href="" class="button-main">ユーザー間取引 一覧へ</a>
+      <div class="content">
+        <h3 class="index_title">
+          <span class="index_title-en">User Market</span>
+          <span class="index_title-jp">ユーザー間取引</span>
+        </h3>
+        <ul class="userMarket_list">
+          <li class="userMarket_item">
+            <a href="">
+              <figure class="userMarket_img">
+                <img
+                  src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
+                  alt=""
+                />
+              </figure>
+              <div class="userMarket_title">パラボラ</div>
+              <div class="userMarket_artist">Official髭男dism</div>
+              <div class="userMarket_price">
+                最安値：
+                <span class="price">1,500</span>
+                円
+              </div>
+            </a>
+          </li>
+          <li class="userMarket_item">
+            <a href="">
+              <figure class="userMarket_img">
+                <img
+                  src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
+                  alt=""
+                />
+              </figure>
+              <div class="userMarket_title">パラボラ</div>
+              <div class="userMarket_artist">Official髭男dism</div>
+              <div class="userMarket_price">
+                最安値：
+                <span class="price">1,500</span>
+                円
+              </div>
+            </a>
+          </li>
+          <li class="userMarket_item">
+            <a href="">
+              <figure class="userMarket_img">
+                <img
+                  src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
+                  alt=""
+                />
+              </figure>
+              <div class="userMarket_title">パラボラ</div>
+              <div class="userMarket_artist">Official髭男dism</div>
+              <div class="userMarket_price">
+                最安値：
+                <span class="price">1,500</span>
+                円
+              </div>
+            </a>
+          </li>
+          <li class="userMarket_item">
+            <a href="">
+              <figure class="userMarket_img">
+                <img
+                  src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
+                  alt=""
+                />
+              </figure>
+              <div class="userMarket_title">パラボラ</div>
+              <div class="userMarket_artist">Official髭男dism</div>
+              <div class="userMarket_price">
+                最安値：
+                <span class="price">1,500</span>
+                円
+              </div>
+            </a>
+          </li>
+          <li class="userMarket_item">
+            <a href="">
+              <figure class="userMarket_img">
+                <img
+                  src="https://pbs.twimg.com/media/EWCIGfqVAAQ8q9A?format=jpg&name=4096x4096"
+                  alt=""
+                />
+              </figure>
+              <div class="userMarket_title">パラボラ</div>
+              <div class="userMarket_artist">Official髭男dism</div>
+              <div class="userMarket_price">
+                最安値：
+                <span class="price">1,500</span>
+                円
+              </div>
+            </a>
+          </li>
+        </ul>
+        <div class="pickupAuction_link">
+          <a href="" class="button-main">ユーザー間取引 一覧へ</a>
+        </div>
       </div>
     </section>
     <section class="news">
-      <h3 class="index_title">
-        <span class="index_title-en">News</span>
-        <span class="index_title-jp">
-          お知らせ
-          <a href="">一覧</a>
-        </span>
-      </h3>
-      <ul class="news_list">
-        <li class="news_item">
-          <a href="">
-            <span class="news_date">2020.09.01</span>
-            BATONサービスをスタートしました。
-          </a>
-        </li>
-      </ul>
+      <div class="content">
+        <h3 class="index_title">
+          <span class="index_title-en">News</span>
+          <span class="index_title-jp">
+            お知らせ
+            <a href="">一覧</a>
+          </span>
+        </h3>
+        <ul class="news_list">
+          <li class="news_item">
+            <a href="">
+              <span class="news_date">2020.09.01</span>
+              BATONサービスをスタートしました。
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
     <section class="about">
-      <h3 class="index_title">
-        <span class="index_title-en">What is “Music royalty”</span>
-      </h3>
-      <h4 class="about_title"></h4>
-      <p class="about_text"></p>
-      <a class="button-main" href="">もっと詳しく</a>
+      <div class="content">
+        <h3 class="index_title">
+          <span class="index_title-en">What is “Music royalty”</span>
+        </h3>
+        <div class="about_wrap">
+          <h4 class="about_title">
+            音楽著作権が資産になる、<br />
+            新しい投資スタイル
+          </h4>
+          <p class="about_text">
+            好きな音楽の著作権をオークション形式で落札してあなたのものに。<br />
+            保有する権利で得られた利益が配当されます。権利は株式で分配されるので、少額から投資を始めることができます。
+          </p>
+          <a class="button-main" href="">もっと詳しく</a>
+        </div>
+      </div>
     </section>
+    <div class="indexFooter">
+      <p class="indexFooter_text">
+        あなたも、好きな音楽に投資を始めてみませんか
+      </p>
+      <a class="button-action" href="">もっと詳しく</a>
+    </div>
   </div>
 </template>
 
