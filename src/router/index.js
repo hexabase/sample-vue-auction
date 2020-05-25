2cab654ad26e7cdf88f911a5f8f8bf536e885875
asnet-tomimoto <akihumi.tomimoto@asnet.co.jp>
2020-05-25 17:21:25 +0900

    ページ遷移によるスクロール位置をネイティブ仕様に変更

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,14 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 });
 
 // 未認証の場合はログイン画面へ
