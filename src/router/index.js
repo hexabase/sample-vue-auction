46461dd9b37e1642c32dc4c801192b08e440de89
asnet-tomimoto <akihumi.tomimoto@asnet.co.jp>
2020-05-28 11:46:37 +0900

    コード整形

--- src/router/index.js
+++ src/router/index.js
@@ -43,11 +43,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0, y: 0 };
     }
   }
 });
