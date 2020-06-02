e2ec39945a54bb0894af6d444527e0b2dee87e73
asnet-tomimoto <akihumi.tomimoto@asnet.co.jp>
2020-06-02 10:33:59 +0900

    ・取引中オークションタブ初期表示にv-tab--activeクラス追加 ・オークション情報取得の条件を、オークション状況からオークション終了時間で見るよう変更

--- views/AuctionList.vue
+++ views/AuctionList.vue
@@ -13,7 +13,7 @@
         <li class="pageHeader_tabItem">オークション終了</li>
       </ul> -->
       <v-tabs hide-slider class="tabMenu">
-        <v-tab :to="{ name: 'Auctionlist' }" class="tabMenu_tabItem">
+        <v-tab :to="{ name: 'Auctionlist' }" class="tabMenu_tabItem v-tab--active">
           取引中オークション
         </v-tab>
         <v-tab :to="{ name: 'ClosedAuction' }" class="tabMenu_tabItem">
@@ -88,6 +88,7 @@
   </div>
 </template>
 <script>
+import moment from "moment-timezone";
 export default {
   data() {
     return {
@@ -156,9 +157,12 @@ export default {
         {
           conditions: [
             {
-              id: "オークション状況", // Hexalink画⾯で⼊⼒したIDを指定
-              search_value: ["オークション中"],
-              exact_match: true // 完全⼀致で検索
+              id: "オークション終了時間", // Hexalink画⾯で⼊⼒したIDを指定
+              search_value: [
+                moment(),
+                null
+              ],
+              exact_match: false // 完全⼀致で検索
             }
           ],
           page: 1,
@@ -167,7 +171,7 @@ export default {
         }
       );
     },
-    async getFinishedAuctionList() {
+    async getClosedAuctionList() {
       return await this.$hexalink.getItems(
         this.token,
         this.applicationId,
