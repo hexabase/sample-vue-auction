ba11d6386daf67e0702cee31d0a1e6b468234287
asnet-tomimoto <akihumi.tomimoto@asnet.co.jp>
2020-07-22 15:21:36 +0900

    ・都道府県追加

--- src/components/parts/form/FormAddress.vue
+++ src/components/parts/form/FormAddress.vue
@@ -58,12 +58,12 @@
           class="prefectures"
         >
           <v-select
-            :items="['東京都', '神奈川県', '千葉県', '埼玉県']"
+            :items="prefecturesItems"
             dense
             outlined
             single-line
             label="都道府県を選択"
-            :value="value"
+            :value="value.prefectures"
             :disabled="!editable"
             :error-messages="errors"
             @input="inputValue"
@@ -154,11 +154,64 @@ export default {
     address2rule: {
       type: String,
       default: ""
+    },
+    prefecturesrule: {
+      type: String,
+      default: ""
     }
   },
   data() {
     return {
-      zip1: ""
+      zip1: "",
+      prefecturesItems: [
+        "北海道",
+        "青森県",
+        "岩手県",
+        "宮城県",
+        "秋田県",
+        "山形県",
+        "福島県",
+        "茨城県",
+        "栃木県",
+        "群馬県",
+        "埼玉県",
+        "千葉県",
+        "東京都",
+        "神奈川県",
+        "新潟県",
+        "富山県",
+        "石川県",
+        "福井県",
+        "山梨県",
+        "長野県",
+        "岐阜県",
+        "静岡県",
+        "愛知県",
+        "三重県",
+        "滋賀県",
+        "京都府",
+        "大阪府",
+        "兵庫県",
+        "奈良県",
+        "和歌山県",
+        "鳥取県",
+        "島根県",
+        "岡山県",
+        "広島県",
+        "山口県",
+        "徳島県",
+        "香川県",
+        "愛媛県",
+        "高知県",
+        "福岡県",
+        "佐賀県",
+        "長崎県",
+        "熊本県",
+        "大分県",
+        "宮崎県",
+        "鹿児島県",
+        "沖縄県"
+      ]
     };
   },
   methods: {
