d0dab15e1ec6048b8d5827c3c5472731462accc6
Seiko Horiuchi <seikohoriuchi@SeikonoMacBook-Pro.local>
2020-11-04 16:07:32 +0900

    お知らせにページング追加

--- views/News.vue
+++ views/News.vue
@@ -13,27 +13,99 @@
           プレスリリース
         </h3>
         <div class="news_list">
-          <article class="newsItem">
-            <p class="newsItem_date">2020/12/01</p>
-            <h4 class="newsItem_title">BATONサービスを開始しました</h4>
+          <article
+            v-for="(news, index) in displayNewsList"
+            :key="index"
+            class="newsItem"
+          >
+            <p class="newsItem_date">{{ news.日付 | moment("YYYY.MM.DD") }}</p>
+            <h4 class="newsItem_title">{{ news.タイトル }}</h4>
             <div class="newsItem_content">
               <p>
-                ああああああああああああああああああああああああああ
+                {{ news.内容 }}
               </p>
             </div>
           </article>
+          <v-pagination
+            v-model="page"
+            :length="length"
+            :total-visible="7"
+            @input="pageChange"
+          ></v-pagination>
         </div>
       </div>
     </section>
   </div>
 </template>
 <script>
+import mapping from "@/assets/json/auctionDBMapping.json";
+import moment from "moment-timezone";
+
 export default {
+  filters: {
+    moment(value, format) {
+      return moment(value)
+        .tz("Asia/Tokyo")
+        .format(format);
+    }
+  },
   data() {
-    return {};
+    return {
+      page: 1,
+      length: 0,
+      pageSize: 10,
+      newsList: [],
+      displayNewsList: [],
+      mapping: JSON.parse(JSON.stringify(mapping))
+    };
   },
   created: async function() {},
-  mounted: async function() {},
-  methods: {}
+  mounted: async function() {
+    this.newsList = new Array(35).fill().map((v, i) => {
+      return {
+        日付: "2020-11-04T00:53:07Z",
+        タイトル: "ああああ" + i,
+        内容: "いいい" + i
+      };
+    });
+    this.length = Math.ceil(this.newsList.length / this.pageSize);
+    this.displayNewsList = this.newsList.slice(0, this.pageSize);
+
+    try {
+      this.$store.commit("common/setLoading", true);
+      this.newsList = await this.getNewsList();
+      this.length = Math.ceil(this.newsList.length / this.pageSize);
+      this.displayNewsList = this.newsList.slice(0, this.pageSize);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.$store.commit("common/setLoading", false);
+    }
+  },
+  methods: {
+    async getNewsList() {
+      return await this.$hexalink.getPublicItems(
+        this.mapping.applicationId,
+        this.mapping.table.ニューステーブル,
+        {
+          page: 1,
+          per_page: 0,
+          use_display_id: true,
+          sort_field_id: "日付",
+          sort_order: "asc"
+        }
+      );
+    },
+    pageChange: function(pageNumber) {
+      this.displayNewsList = this.newsList.slice(
+        this.pageSize * (pageNumber - 1),
+        this.pageSize * pageNumber
+      );
+      window.scrollTo({
+        top: 190,
+        behavior: "smooth"
+      });
+    }
+  }
 };
 </script>
