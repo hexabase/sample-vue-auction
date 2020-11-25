49bb5cc8604e40e3b1ad85eb4c45c039988de27f
Seiko Horiuchi <seikohoriuchi@SeikonoMacBook-Pro.local>
2020-11-25 10:38:13 +0900

    規約系ページ追加

--- src/components/globals/Footer.vue
+++ src/components/globals/Footer.vue
@@ -10,14 +10,40 @@
         </a>
       </h2>
       <ul class="siteFooter_nav" role="navigation">
-        <li class="siteFooter_nav-link"><a href="">運営会社</a></li>
-        <li class="siteFooter_nav-link"><a href="">○○方針</a></li>
-        <li class="siteFooter_nav-link"><a href="">○○約款</a></li>
-        <li class="siteFooter_nav-link"><a href="">○○指針</a></li>
+        <li class="siteFooter_nav-link">
+          <router-link to="/">
+            運営会社
+          </router-link>
+        </li>
+        <li class="siteFooter_nav-link">
+          <router-link to="/solicitation">
+            勧誘方針
+          </router-link>
+        </li>
+        <li class="siteFooter_nav-link">
+          <router-link to="/privacy">
+            プライバシーポリシー
+          </router-link>
+        </li>
+        <li class="siteFooter_nav-link">
+          <router-link to="/antisocial">
+            反社会的勢力への対応に関する基本方針
+          </router-link>
+        </li>
+        <li class="siteFooter_nav-link">
+          <router-link to="/terms">
+            利用規約
+          </router-link>
+        </li>
         <li class="siteFooter_nav-link">
           <router-link to="/contact">お問い合わせ</router-link>
         </li>
       </ul>
+      <dl class="siteFooter_company-info">
+        <dt>SYZYGY株式会社</dt>
+        <dd>第二種金融商品取引業者&nbsp;&nbsp;関東財務局長（金商）第●号</dd>
+        <dd>加入協会：一般社団法人第二種金融商品取引業協会</dd>
+      </dl>
       <small class="siteFooter_copy">
         Copyright 2020 BATON All Rights Reserved.
       </small>
